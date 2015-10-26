ea0444323c16280563f6484fb598f5450a8643c8
Max Gurela <maxpowa1@gmail.com>
2015-10-26 12:27:36 -0400

    v0.1.2

--- sopel_modules/github/__init__.py
+++ sopel_modules/github/__init__.py
@@ -9,6 +9,6 @@ from .github import *
 
 __author__ = 'maxpowa'
 __email__ = 'maxpowa@outlook.com'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 __repo__ = 'https://github.com/maxpowa/sopel-github'
 
