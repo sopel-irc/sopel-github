1923b1cdb863008a2a57a91e3fe2ca0bd42b35fc
dgw <dgw@technobabbl.es>
2019-03-28 08:57:59 -0500

    webhook: prevent double notifications on PR review comment replies
    GitHub is silly to fire off empty "review" events with state "commented"
    when someone replies to an existing review comment, but since we can't
    turn that off we have to handle the case instead...

--- sopel_modules/github/formatting.py
+++ sopel_modules/github/formatting.py
@@ -463,7 +463,12 @@ def get_formatted_response(payload, row):
         elif re.match('(labeled|unlabeled)', payload['action']):
             messages.append(fmt_issue_label_message() + " " + fmt_url(shorten_url(payload['pull_request']['html_url'])))
     elif payload['event'] == 'pull_request_review':
-        if payload['action'] == 'submitted' and payload['review']['state'] != 'pending':
+        if payload['action'] == 'submitted' and payload['review']['state'] in ['approved', 'changes_requested', 'commented']:
+            if payload['review']['state'] == 'commented' and payload['review']['body'] == None:
+                # Probably an empty "review" fired by a pull_request_review_comment reply, which we'll get in a separate hook delivery.
+                # Wish GitHub didn't fire both events, but they do, even though it makes no sense.
+                # Either way, an empty review must be accompanied by comments, which will get handled when their hook(s) fire(s).
+                return
             messages.append(fmt_pull_request_review_summary_message() + " " + fmt_url(shorten_url(payload['review']['html_url'])))
         elif payload['action'] == 'dismissed':
             messages.append(fmt_pull_request_review_dismissal_message() + " " + fmt_url(shorten_url(payload['review']['html_url'])))
