3e44fd17e344cdd425ce7b1ba3c139cd86deb24a
dgw <dgw@technobabbl.es>
2019-05-12 19:26:19 -0500

    Prepare 0.2.0a13

--- sopel_modules/github/__init__.py
+++ sopel_modules/github/__init__.py
@@ -9,6 +9,6 @@ from .github import *
 
 __author__ = 'maxpowa'
 __email__ = 'maxpowa@outlook.com'
-__version__ = '0.2.0a12'
+__version__ = '0.2.0a13'
 __repo__ = 'https://github.com/sopel-irc/sopel-github'
 
